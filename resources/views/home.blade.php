9a15c4d9abe03df642d2db0ebfb95aef2618f4a7
ahmad.obeidat <ahmad.obeidat@olahub.com>
2021-07-24 15:56:03 +0300

    referabell code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,57 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        @extends('layouts.sidebar')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
     </div>
 </div>
+<script>
+    // -------multilevel-accordian-menu---------
+$(document).ready(function() {
+	$("#accordian a").click(function() {
+		var link = $(this);
+		var closest_ul = link.closest("ul");
+		var parallel_active_links = closest_ul.find(".active")
+		var closest_li = link.closest("li");
+		var link_status = closest_li.hasClass("active");
+		var count = 0;
+
+		closest_ul.find("ul").slideUp(function() {
+			if (++count == closest_ul.find("ul").length){
+				parallel_active_links.removeClass("active");
+				parallel_active_links.children("ul").removeClass("show-dropdown");
+				link.not().children("ul").removeClass(".active");
+			}
+		});
+
+		if (!link_status) {
+				closest_li.children("ul").slideDown().addClass("show-dropdown");
+				closest_li.addClass("active");
+		}
+
+	})
+});
+
+
+
+
+
+
+// --------for-active-class-on-other-page-----------
+jQuery(document).ready(function($){
+  	// Get current path and find target link
+  	var path = window.location.pathname.split("/").pop();
+
+  	// Account for home page with empty path
+  	if ( path == '' ) {
+    	path = 'index.html';
+  	}
+
+  	var target = $('#accordian li a[href="'+path+'"]');
+  	// Add active class to target link
+  	target.parents("li").addClass('active');
+  	target.parents("ul").addClass("show-dropdown");
+});
+
+</script>
 @endsection
